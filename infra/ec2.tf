62e2183c51fe7998355ae9d5e754762496f19919
larissa <larissasgandarela@gmail.com>
2025-02-27 18:03:22 -0300

    correcao pipeline

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "go_app" {
-  ami                    = "ami-0a31834d359d68156"
+  ami                    = "ami-014d544cfef21b42d"
   instance_type          = "t2.micro"
   key_name               = "par-de-chaves"
   security_groups        = [aws_security_group.go_app_sg.name]
