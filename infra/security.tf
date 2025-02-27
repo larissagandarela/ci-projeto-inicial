ee5a60e3fdaf56193605d8bc74b2b028da61e9ef
larissa gandarela <65129814+larissagandarela@users.noreply.github.com>
2025-02-27 14:03:58 -0300

    Update security.tf

--- infra/security.tf
+++ infra/security.tf
@@ -1,12 +1,7 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_security_group" "go_app_sg" {
   name        = "go_app_sg"
   description = "Security group for Go app EC2 instance"
-  vpc_id      = aws_vpc.main.id
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
